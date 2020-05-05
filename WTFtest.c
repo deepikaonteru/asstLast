e45a342e0598b5037d6e023b76456f96946341ad
Deepika Onteru <do230@ilab3.cs.rutgers.edu>
2020-05-05 17:44:28 -0400

    tester

--- WTFtest.c
+++ WTFtest.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+
+void  ClientProcess(void)
 {
-    
+    system("./WTF configure 127.0.0.1 9123");
+
+    system("./WTF create WTFProject")
+}
+
+void  ServerProcess(void)
+{
+    system("./WTF 9123");
+
+}
+
+
+void  main(void)
+{
+     pid_t  pid;
+
+     pid = fork();
+     if (pid == 0) 
+          ClientProcess();
+     else 
+          ServerProcess();
 }
